d10ca98f0031902c165f75d85194772c291f1331
VVasyanovich <vvasyanovich@plslogistics.com>
2012-09-27 18:57:58 +0300

    habra post

--- src/ua/pp/keebraa/vktimer/core/Application.java
+++ src/ua/pp/keebraa/vktimer/core/Application.java
@@ -2,7 +2,7 @@ package ua.pp.keebraa.vktimer.core;
 
 import ua.pp.keebraa.vktimer.api.AccessTokenGetter;
 import ua.pp.keebraa.vktimer.api.UserOnlineStatus;
-
+//http://habrahabr.ru/post/131943/
 public class Application {
 	public void init() {
 
@@ -12,7 +12,7 @@ public class Application {
 		AccessTokenGetter tokenGetter = new AccessTokenGetter();
 		String token = tokenGetter.getAccessToken();
 		UserOnlineStatus status = new UserOnlineStatus();
-		boolean isOnline = status.isOnline("163730895", token);
+		boolean isOnline = status.isOnline("blablabla_whiskas", token);
 		System.out.println(isOnline);
 	}
 }
