a58556e5b37c00f101eac9a381e2e249e0fb7d58
vvasyanovich <vvasyanovich@plslogistics.com>
2012-09-27 10:15:25 +0300

    initial commit.

---
+++ src/ua/pp/keebraa/vktimer/core/Main.java
@@ -0,0 +1,56 @@
+package ua.pp.keebraa.vktimer.core;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import chrriis.common.UIUtils;
+import chrriis.dj.nativeswing.swtimpl.NativeInterface;
+import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
+
+public class Main extends JPanel {
+
+  public Main() {
+    super(new BorderLayout());
+    JPanel webBrowserPanel = new JPanel(new BorderLayout());
+    webBrowserPanel.setBorder(BorderFactory.createTitledBorder("Native Web Browser component"));
+    final JWebBrowser webBrowser = new JWebBrowser();
+    webBrowser.navigate("http://api.vk.com/oauth/authorize?client_id=3145529&redirect_uri=http://api.vk.com/blank.html&scope=notify,friends,photos,audio,video,docs,notes,pages,wall,groups,messages,ads&display=page&response_type=token");
+    webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
+    add(webBrowserPanel, BorderLayout.CENTER);
+    // Create an additional bar allowing to show/hide the menu bar of the web browser.
+    JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 4, 4));
+    JCheckBox menuBarCheckBox = new JCheckBox("Menu Bar", webBrowser.isMenuBarVisible());
+    menuBarCheckBox.addItemListener(new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        webBrowser.setMenuBarVisible(e.getStateChange() == ItemEvent.SELECTED);
+      }
+    });
+    buttonPanel.add(menuBarCheckBox);
+    add(buttonPanel, BorderLayout.SOUTH);
+  }
+
+  /* Standard main method to try that test as a standalone application. */
+  public static void main(String[] args) {
+    UIUtils.setPreferredLookAndFeel();
+    NativeInterface.open();
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        JFrame frame = new JFrame("DJ Native Swing Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new Main(), BorderLayout.CENTER);
+        frame.setSize(800, 600);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+      }
+    });
+    NativeInterface.runEventPump();
+  }
+}
