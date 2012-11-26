b53a80987d0dc07fdb302ac6ee11e682f93d407a
VVasyanovich <vvasyanovich@plslogistics.com>
2012-11-26 18:47:49 +0200

    add javaDoc

--- src/ua/pp/keebraa/vktimer/api/VkApiFactory.java
+++ src/ua/pp/keebraa/vktimer/api/VkApiFactory.java
@@ -3,6 +3,9 @@ package ua.pp.keebraa.vktimer.api;
 import ua.pp.keebraa.vktimer.api.interfaces.IVkApi;
 
 public class VkApiFactory {
+	/** 
+	/* provides exception-save api
+	**/
     public IVkApi getExceptionSafeApi() {
         return null;
     }
